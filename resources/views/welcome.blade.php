cb593756c1f42a4a10bbdda87b697dc64099d670
RixhersAjazi <coderrix@gmail.com>
2016-06-25 02:35:46 -0400

    updated git ignore and now we have homesteadgit status

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Home of AlVirt. Authors @coderrix @clayton</div>
+                <div class="title">Home of AlVirt. <br>Authors @coderrix @clayton</div>
             </div>
         </div>
     </body>
